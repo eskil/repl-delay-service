877cbaebb98bfb191769275559876b7b9818a32f
Eskil Olsen <eskil@uber.com>
2014-11-13 07:36:49 +0000

    Initial commit

---
+++ src/repl_delay_app.erl
@@ -0,0 +1,16 @@
+-module(repl_delay_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    repl_delay_sup:start_link().
+
+stop(_State) ->
+    ok.
